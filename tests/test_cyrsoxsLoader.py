fbfb73e71d407240f784eab06d91e3c36d566634
Peter Beaucage <peter.beaucage@nist.gov>
2022-12-12 13:37:08 -0500

    Typo fix in test

--- tests/test_cyrsoxsLoader.py
+++ tests/test_cyrsoxsLoader.py
@@ -5,6 +5,7 @@ from PyHyperScattering.load import cyrsoxsLoader
 
 import xarray as xr
 import pathlib
+import pytest
 #import HDR
 
 @pytest.fixture(autouse=True,scope='module')
@@ -16,4 +17,4 @@ def cyrsoxs_single_scan(cyrsoxs_loader):
     return cyrsoxs_loader.loadDirectory(pathlib.Path('2021-09-03_pl-0.1_cyl10_core5_sp60'))
 
 def test_cyrsoxs_single_scan_import(cyrsoxs_single_scan):
-    assert type(raw) == xr.DataArray
+    assert type(cyrsoxs_single_scan) == xr.DataArray
