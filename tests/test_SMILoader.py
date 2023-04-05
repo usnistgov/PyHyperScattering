bb34fa6ec380934aa3fba02e507e5435981cfb18
Peter Beaucage <github@peterbeaucage.com>
2023-04-05 12:38:06 -0400

    Another syntax fix

--- tests/test_SMILoader.py
+++ tests/test_SMILoader.py
@@ -16,7 +16,7 @@ def smiloader():
 
 @pytest.fixture(autouse=True,scope='module')
 def SMI_single_scan(smiloader):
-    return smiloader.loadFileSeries('smi_example/PN_AGBEH_',['energy','polarization'])
+    return smiloader.loadDirectory('smi_example/PN_AGBEH_')
 
 def test_SMI_single_scan_import(SMI_single_scan):
     assert type(SMI_single_scan)==xr.DataArray
