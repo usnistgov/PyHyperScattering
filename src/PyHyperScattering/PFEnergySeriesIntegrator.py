4892704a7b21aa8b950723103f2169412d2c3d49
Peter Beaucage <peter.beaucage@nist.gov>
2023-04-06 15:08:31 -0400

    Add a little more bulletproofing for single images

--- src/PyHyperScattering/PFEnergySeriesIntegrator.py
+++ src/PyHyperScattering/PFEnergySeriesIntegrator.py
@@ -40,6 +40,8 @@ class PFEnergySeriesIntegrator(PFGeneralIntegrator):
         #    get the energy and locate the matching integrator
         #    use that integrator to reduce
         #    return single reduced frame
+        
+        
         if type(img.energy) != float:
             try:
                 en = img.energy.values[0]
@@ -66,7 +68,7 @@ class PFEnergySeriesIntegrator(PFGeneralIntegrator):
                 return res.interp(q=self.dest_q)
             else:
                 return res
-        except TypeError:
+        except (TypeError,AttributeError):
             return res
     def setupIntegrators(self,energies):
         '''
@@ -99,7 +101,7 @@ It would cosmically be better (for things like resolution calculation) to have t
         # idx_name_to_use = 'energy'#indexes[0]
         # idx_val_to_use = img_stack.indexes[idx_name_to_use]
         
-        
+
         if 'energy' in indexes:
             dim_to_chunk = 'energy'
         else:
@@ -173,6 +175,8 @@ It would cosmically be better (for things like resolution calculation) to have t
             if img_stack.__getattr__(indexes[0]).to_pandas().drop_duplicates().shape[0] != img_stack.__getattr__(indexes[0]).shape[0]:
                 warnings.warn(f'Axis {indexes[0]} contains duplicate conditions.  This is not supported and may not work.  Try adding additional coords to separate image conditions',stacklevel=2)
             data_int = data.groupby(indexes[0],squeeze=False).progress_apply(self.integrateSingleImage)
+        elif len(indexes) == 0:
+            data_int = self.integrateSingleImage(data).isel(image_num=0)
         else:
             #some kinda logic to check for existing multiindexes and stack into them appropriately maybe
             data = data.stack({'pyhyper_internal_multiindex':indexes})
