e66881b27f7b70e17d8146e48e5204be69a4afaf
Peter Beaucage <peter.beaucage@nist.gov>
2023-04-07 11:20:28 -0400

    Performance optimization in beamcentering

--- src/PyHyperScattering/BeamCentering.py
+++ src/PyHyperScattering/BeamCentering.py
@@ -3,6 +3,7 @@ import numpy as np
 import xarray as xr
 from pyFAI import azimuthalIntegrator
 from scipy.optimize import minimize
+from tqdm.auto import tqdm
 
 
 @xr.register_dataarray_accessor('util')
@@ -20,7 +21,11 @@ class CenteringAccessor:
         self.integrator = None
         self.centering_energy = None
 
-    def create_integrator(self, energy):
+    def create_integrator(self, energy,poni1=None,poni2=None):
+        if poni1 == None:
+            poni1 = self._obj.poni1
+        if poni2 == None:
+            poni2 = self._obj.poni2
         return azimuthalIntegrator.AzimuthalIntegrator(
             self._obj.dist, self._obj.poni1, self._obj.poni2,
             self._obj.rot1, self._obj.rot2, self._obj.rot3,
@@ -28,24 +33,26 @@ class CenteringAccessor:
             wavelength=1.239842e-6/energy
         )
 
-    def optimization_func(self, x, image, integrator,
-                          q_min, q_max,
+    def optimization_func(self, x, image, obj, energy,
+                          q_min, q_max, pbar=None,
                           chi_min=-179, chi_max=179,
                           num_points=150):
-        poni1, poni2 = x
-        integrator.poni1 = poni1
-        integrator.poni2 = poni2
-        _, image_int = integrator.integrate_radial(image, num_points,
+        #print(f'evaluating objective at {x}')
+        obj.create_integrator(energy,poni1=x[0],poni2=x[1])
+        _, image_int = obj.integrator.integrate_radial(image, num_points,
                                                    radial_range=(q_min, q_max),
                                                    azimuth_range=(chi_min, chi_max),
                                                    radial_unit='q_A^-1')
+        if pbar is not None:
+            pbar.update(1)
         return np.var(image_int[image_int != 0])
 
     def refine_geometry(self, energy, q_min, q_max,
                         chi_min=-179, chi_max=179,
                         poni1_guess=None, poni2_guess=None,
                         bounds=None, method='Nelder-Mead',
-                        num_points=150):
+                        num_points=150, max_iter = 150, 
+                        verbose=False, ):
         if not poni1_guess:
             poni1_guess = self._obj.poni1
             poni2_guess = self._obj.poni2
@@ -55,15 +62,20 @@ class CenteringAccessor:
         if not bounds:
             bounds = [(poni1_guess*0.9, poni1_guess*1.1),
                       (poni2_guess*0.9, poni2_guess*1.1)]
-        image = self._obj.unstack('system').sel(energy=energy).values
-        res = minimize(self.optimization_func, (poni1_guess, poni2_guess),
-                       args=(image, self.integrator,
-                             q_min, q_max, chi_min, chi_max,
-                             num_points),
-                       bounds=bounds, method=method)
+        options = {}
+        options['maxiter'] = max_iter
+        if verbose:
+            options['disp'] = True
+        image = self._obj.sel(energy=energy).values
+        with tqdm(total=1,desc='Optimizing Beamcenter') as pbar:
+            res = minimize(self.optimization_func, (poni1_guess, poni2_guess),
+                           args=(image, self, energy,
+                                 q_min, q_max, pbar, chi_min, chi_max,
+                                 num_points),
+                           bounds=bounds, method=method, options=options)
 
         if res.success:
-            print('Optimization successful. Updating beamcenter')
+            print(f'Optimization successful. Updating beamcenter to ({res.x[0]},{res.x[1]}), old values were ({self._obj.poni1},{self._obj.poni2})')
             self._obj.attrs['poni1'] = res.x[0]
             self._obj.attrs['poni2'] = res.x[1]
         else:
