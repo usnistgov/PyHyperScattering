c60d4d16317ef70bee49d30743dbdb24fa33b0f5
Andrew Levin <andrew.levin@colorado.edu>
2023-07-08 11:20:53 -0600

    add GIWAXS.py to util.py

--- src/PyHyperScattering/util.py
+++ src/PyHyperScattering/util.py
@@ -5,3 +5,4 @@ from PyHyperScattering import IntegrationUtils
 #from PyHyperScattering import Nexus empty module as of 0.0.6-dev69
 from PyHyperScattering import FileIO
 from PyHyperScattering import PlotTools
+from PyHyperScattering import GIWAXS
