aba83f74ba00a2f11ae4a45c22fc20a5474a1f47
Andrew Levin <andrew.levin@colorado.edu>
2023-07-13 02:02:03 -0600

    more adjustments to drawmask

--- src/PyHyperScattering/IntegrationUtils.py
+++ src/PyHyperScattering/IntegrationUtils.py
@@ -114,7 +114,7 @@ class DrawMask:
 
     '''
     
-    def __init__(self,frame, cmap='viridis', clim=(5e0, 5e3)):
+    def __init__(self,frame, cmap='viridis', clim=(5e0, 5e3), width=800, height=700):
         '''
         Construct a DrawMask object
 
@@ -129,7 +129,8 @@ class DrawMask:
         self.frame = frame
         
         self.fig = frame.hvplot(cmap=cmap, clim=clim, logz=True, data_aspect=1, 
-                                x=frame.dims[1], y=frame.dims[0])
+                                x=frame.dims[1], y=frame.dims[0], width=width, 
+                                height=height)
 
         self.poly = hv.Polygons([])
         self.path_annotator = hv.annotate.instance()
@@ -144,10 +145,7 @@ class DrawMask:
         '''
         print('Usage: click the "PolyAnnotator" tool at top right.  DOUBLE CLICK to start drawing a masked object, SINGLE CLICK to add a vertex, then DOUBLE CLICK to finish.  Click/drag individual vertex to adjust.')
         return self.path_annotator(
-                self.fig * self.poly.opts(
-                            width=self.frame.shape[1], 
-                            height=self.frame.shape[0], 
-                            responsive=False), 
+                self.fig * self.poly.opts(responsive=False), 
                 annotations=['Label'], 
             vertex_annotations=['Value'])
 
