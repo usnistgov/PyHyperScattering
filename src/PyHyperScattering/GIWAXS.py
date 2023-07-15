95e445467d321af76a9144172245caecc2a1d23d
Andrew Levin <andrew.levin@colorado.edu>
2023-07-15 03:32:42 -0600

    make Transform class for pg functions

--- src/PyHyperScattering/GIWAXS.py
+++ src/PyHyperScattering/GIWAXS.py
@@ -13,83 +13,169 @@ import pygix  # type: ignore
 from PyHyperScattering.IntegrationUtils import DrawMask
 from tqdm.auto import tqdm 
 
-def pg_convert(da, poniPath, maskPath, inplane_config='q_xy'):
-    """
-    Converts raw GIWAXS detector image to q-space data. Returns two DataArrays, Qz vs Qxy & Q vs Chi
-    
-    Inputs: Raw GIWAXS DataArray
-            Path to .poni file for converting to q-space & applying missing wedge correction
-    Outputs: Cartesian & Polar DataArrays
-    """
+class Transform:
 
-    # Initialize pygix transform object
-    pg = pygix.Transform()
-    pg.load(str(poniPath))
-    pg.sample_orientation = 3
-    pg.incident_angle = float(da.incident_angle[2:])
+    def __init__(self, poniPath, maskPath, inplane_config='q_xy'):
+        self.poniPath = poniPath
+        self.maskPath = maskPath
+        self.inplane_config = inplane_config
 
-    # Load PyHyper-drawn mask
-    draw = DrawMask(da)
-    draw.load(maskPath)
-    mask = draw.mask
+    def pg_convert(self, da):
+        """
+        Converts raw GIWAXS detector image to q-space data. Returns two DataArrays, Qz vs Qxy & Q vs Chi
+        
+        Inputs: Raw GIWAXS DataArray
+                Path to .poni file for converting to q-space & applying missing wedge correction
+        Outputs: Cartesian & Polar DataArrays
+        """
 
-    recip_data, qxy, qz = pg.transform_reciprocal(da.data,
-                                                  method='bbox',
-                                                  unit='A',
-                                                  mask=mask,
-                                                  correctSolidAngle=True)
-    
-    recip_da = xr.DataArray(data=recip_data,
-                            dims=['q_z', inplane_config],
+        # Initialize pygix transform object
+        pg = pygix.Transform()
+        pg.load(str(self.poniPath))
+        pg.sample_orientation = 3
+        pg.incident_angle = float(da.incident_angle[2:])
+
+        # Load PyHyper-drawn mask
+        draw = DrawMask(da)
+        draw.load(self.maskPath)
+        mask = draw.mask
+
+        recip_data, qxy, qz = pg.transform_reciprocal(da.data,
+                                                      method='bbox',
+                                                      unit='A',
+                                                      mask=mask,
+                                                      correctSolidAngle=True)
+        
+        recip_da = xr.DataArray(data=recip_data,
+                                dims=['q_z', self.inplane_config],
+                                coords={
+                                    'q_z': ('q_z', qz, {'units': '1/Å'}),
+                                    self.inplane_config: (self.inplane_config, qxy, {'units': '1/Å'})
+                                },
+                                attrs=da.attrs)
+
+        caked_data, qr, chi = pg.transform_image(da.data, 
+                                                 process='polar',
+                                                 method = 'bbox',
+                                                 unit='q_A^-1',
+                                                 mask=mask,
+                                                 correctSolidAngle=True)
+
+        caked_da = xr.DataArray(data=caked_data,
+                            dims=['chi', 'qr'],
                             coords={
-                                'q_z': ('q_z', qz, {'units': '1/Å'}),
-                                inplane_config: (inplane_config, qxy, {'units': '1/Å'})
+                                'chi': ('chi', chi, {'units': '°'}),
+                                'qr': ('qr', qr, {'units': '1/Å'})
                             },
                             attrs=da.attrs)
+        caked_da.attrs['inplane_config'] = self.inplane_config
 
-    caked_data, qr, chi = pg.transform_image(da.data, 
-                                             process='polar',
-                                             method = 'bbox',
-                                             unit='q_A^-1',
-                                             mask=mask,
-                                             correctSolidAngle=True)
-
-    caked_da = xr.DataArray(data=caked_data,
-                        dims=['chi', 'qr'],
-                        coords={
-                            'chi': ('chi', chi, {'units': '°'}),
-                            'qr': ('qr', qr, {'units': '1/Å'})
-                        },
-                        attrs=da.attrs)
-    caked_da.attrs['inplane_config'] = inplane_config
-
-    if 'time' in da.coords:
-        recip_da = recip_da.assign_coords({'time': float(da.time)})
-        recip_da = recip_da.expand_dims(dim={'time': 1})
-        caked_da = caked_da.assign_coords({'time': float(da.time)})
-        caked_da = caked_da.expand_dims(dim={'time': 1})
-    
-    return recip_da, caked_da
-    
-def pg_convert_series(da, poniPath, maskPath, inplane_config='q_xy'):
-    """
-    Converts raw GIWAXS DataArray to q-space and returns Cartesian & Polar DataArrays
-
-    Inputs: Raw GIWAXS DataArray with a time dimension
-    Outputs: 2 DataArrays in q-space with dimensions (q_z, inplane_config (default is q_xy), time) and (chi, qr, time)
-    """
-    recip_das = []
-    caked_das = []
-    for time in tqdm(da.time):
-        da_slice = da.sel(time=float(time))
-        recip_da_slice, caked_da_slice = pg_convert(da=da_slice, 
-                                                    poniPath=poniPath,
-                                                    maskPath=maskPath,
-                                                    inplane_config=inplane_config)
-        recip_das.append(recip_da_slice)
-        caked_das.append(caked_da_slice)
+        if 'time' in da.coords:
+            recip_da = recip_da.assign_coords({'time': float(da.time)})
+            recip_da = recip_da.expand_dims(dim={'time': 1})
+            caked_da = caked_da.assign_coords({'time': float(da.time)})
+            caked_da = caked_da.expand_dims(dim={'time': 1})
         
-    recip_da_series = xr.concat(recip_das, 'time')
-    caked_da_series = xr.concat(caked_das, 'time')
+        return recip_da, caked_da
+        
+    def pg_convert_series(self, da):
+        """
+        Converts raw GIWAXS DataArray to q-space and returns Cartesian & Polar DataArrays
+
+        Inputs: Raw GIWAXS DataArray with a time dimension
+        Outputs: 2 DataArrays in q-space with dimensions (q_z, inplane_config (default is q_xy), time) and (chi, qr, time)
+        """
+        recip_das = []
+        caked_das = []
+        for time in tqdm(da.time):
+            da_slice = da.sel(time=float(time))
+            recip_da_slice, caked_da_slice = self.pg_convert(da=da_slice)
+            recip_das.append(recip_da_slice)
+            caked_das.append(caked_da_slice)
+            
+        recip_da_series = xr.concat(recip_das, 'time')
+        caked_da_series = xr.concat(caked_das, 'time')
+        
+        return recip_da_series, caked_da_series
+
+
+# def pg_convert(da, poniPath, maskPath, inplane_config='q_xy'):
+#     """
+#     Converts raw GIWAXS detector image to q-space data. Returns two DataArrays, Qz vs Qxy & Q vs Chi
     
-    return recip_da_series, caked_da_series
+#     Inputs: Raw GIWAXS DataArray
+#             Path to .poni file for converting to q-space & applying missing wedge correction
+#     Outputs: Cartesian & Polar DataArrays
+#     """
+
+#     # Initialize pygix transform object
+#     pg = pygix.Transform()
+#     pg.load(str(poniPath))
+#     pg.sample_orientation = 3
+#     pg.incident_angle = float(da.incident_angle[2:])
+
+#     # Load PyHyper-drawn mask
+#     draw = DrawMask(da)
+#     draw.load(maskPath)
+#     mask = draw.mask
+
+#     recip_data, qxy, qz = pg.transform_reciprocal(da.data,
+#                                                   method='bbox',
+#                                                   unit='A',
+#                                                   mask=mask,
+#                                                   correctSolidAngle=True)
+    
+#     recip_da = xr.DataArray(data=recip_data,
+#                             dims=['q_z', inplane_config],
+#                             coords={
+#                                 'q_z': ('q_z', qz, {'units': '1/Å'}),
+#                                 inplane_config: (inplane_config, qxy, {'units': '1/Å'})
+#                             },
+#                             attrs=da.attrs)
+
+#     caked_data, qr, chi = pg.transform_image(da.data, 
+#                                              process='polar',
+#                                              method = 'bbox',
+#                                              unit='q_A^-1',
+#                                              mask=mask,
+#                                              correctSolidAngle=True)
+
+#     caked_da = xr.DataArray(data=caked_data,
+#                         dims=['chi', 'qr'],
+#                         coords={
+#                             'chi': ('chi', chi, {'units': '°'}),
+#                             'qr': ('qr', qr, {'units': '1/Å'})
+#                         },
+#                         attrs=da.attrs)
+#     caked_da.attrs['inplane_config'] = inplane_config
+
+#     if 'time' in da.coords:
+#         recip_da = recip_da.assign_coords({'time': float(da.time)})
+#         recip_da = recip_da.expand_dims(dim={'time': 1})
+#         caked_da = caked_da.assign_coords({'time': float(da.time)})
+#         caked_da = caked_da.expand_dims(dim={'time': 1})
+    
+#     return recip_da, caked_da
+    
+# def pg_convert_series(da, poniPath, maskPath, inplane_config='q_xy'):
+#     """
+#     Converts raw GIWAXS DataArray to q-space and returns Cartesian & Polar DataArrays
+
+#     Inputs: Raw GIWAXS DataArray with a time dimension
+#     Outputs: 2 DataArrays in q-space with dimensions (q_z, inplane_config (default is q_xy), time) and (chi, qr, time)
+#     """
+#     recip_das = []
+#     caked_das = []
+#     for time in tqdm(da.time):
+#         da_slice = da.sel(time=float(time))
+#         recip_da_slice, caked_da_slice = pg_convert(da=da_slice, 
+#                                                     poniPath=poniPath,
+#                                                     maskPath=maskPath,
+#                                                     inplane_config=inplane_config)
+#         recip_das.append(recip_da_slice)
+#         caked_das.append(caked_da_slice)
+        
+#     recip_da_series = xr.concat(recip_das, 'time')
+#     caked_da_series = xr.concat(caked_das, 'time')
+    
+#     return recip_da_series, caked_da_series
