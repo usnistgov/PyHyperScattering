a8f71f8fcc42517e746d2c51b2010fe43fe9a27d
kewh5868 <kewh5868@colorado.edu>
2023-07-18 01:07:36 -0600

    refactored GIWAXS.py Transform class to include generalized load_mask() and add_time_coord() methods. previous instance is saved as comments below. Updated CMSGIWAXSLoader() to include a createSampleDictionary() method to construct a sample dictionary from a root_folder, to be able to directly call samples from the stored sample_dict class attribute of the instantiation

--- src/PyHyperScattering/CMSGIWAXSLoader.py
+++ src/PyHyperScattering/CMSGIWAXSLoader.py
@@ -17,8 +17,10 @@ class CMSGIWAXSLoader(FileLoader):
     GIXS Data Loader Class | NSLS-II 11-BM (CMS)
     Used to load single TIFF time-series TIFF GIWAXS images.
     """
-    def __init__(self, md_naming_scheme=[]):
+    def __init__(self, md_naming_scheme=[], root_folder=None):
         self.md_naming_scheme = md_naming_scheme
+        self.root_folder = root_folder
+        self.sample_dict = None
 
     def loadSingleImage(self, filepath):
         """
@@ -114,4 +116,65 @@ class CMSGIWAXSLoader(FileLoader):
 
         return out
 
+    def createSampleDictionary(self, root_folder):
+        """
+        Loads and creates a sample dictionary from a root folder path.
+        The dictionary will contain: sample name, scanID list, series scanID list, 
+        and a pathlib object variable for each sample's data folder (which contains the /maxs/raw/ subfolders).
+        """
 
+        # Ensure the root_folder is a pathlib.Path object
+        self.root_folder = pathlib.Path(self.root_folder)
+        if not self.root_folder.is_dir():
+            raise ValueError(f"Directory {self.root_folder} does not exist.")
+        
+        # Initialize the sample dictionary
+        sample_dict = {}
+
+        # Find the index of 'scan_id' and 'series_number' in the md_naming_scheme list
+        scan_id_index = None
+        series_number_index = None
+        scan_id_aliases = ['scanID', 'ID', 'scannum', 'scan', 'SCAN', 'Scan', 'scanid', 'id', 'ScanNum', 'scan_id', 'scan_ID']
+        series_number_aliases = ['seriesnum', 'seriesid', 'series_id', 'series_ID', 'series', 'SERIES', 'Series', 'series_number', 'series_num']
+
+        for index, name in enumerate(self.md_naming_scheme):
+            if name.lower() in [alias.lower() for alias in scan_id_aliases]:
+                scan_id_index = index
+            elif name.lower() in [alias.lower() for alias in series_number_aliases]:
+                series_number_index = index
+
+        if scan_id_index is None or series_number_index is None:
+            raise ValueError('md_naming_scheme does not contain keys for scan_id or series_number.')
+        
+        # Iterate through all subdirectories in the root folder
+        for subdir in root_folder.iterdir():
+            if subdir.is_dir():
+                # Check if /maxs/raw/ subdirectory exists
+                maxs_raw_dir = subdir / "maxs" / "raw"
+                if maxs_raw_dir.is_dir():
+                    # The name of the subdirectory is considered as the sample name
+                    sample_name = subdir.name
+                    sample_dict[sample_name] = {
+                        "scanlist": [],
+                        "serieslist": {},
+                        "sample_path": subdir
+                    }
+                    
+                    # Iterate through the files in the /maxs/raw/ subdirectory
+                    for filename in maxs_raw_dir.glob('*'):
+                        metadata = filename.stem.split('_')
+                        scan_id = metadata[scan_id_index]
+                        series_number = metadata[series_number_index]
+
+                        # Update serieslist
+                        if scan_id in sample_dict[sample_name]["serieslist"]:
+                            sample_dict[sample_name]["serieslist"][scan_id] += 1
+                        else:
+                            sample_dict[sample_name]["serieslist"][scan_id] = 1
+
+                        # Append the series_number to the scanlist in the dictionary
+                        if series_number not in sample_dict[sample_name]["scanlist"]:
+                            sample_dict[sample_name]["scanlist"].append(series_number)
+
+        self.sample_dict = sample_dict
+        return sample_dict
--- src/PyHyperScattering/GIWAXS.py
+++ src/PyHyperScattering/GIWAXS.py
@@ -1,10 +1,9 @@
 """
 File that will contain functions to:
-    1. Use pygix to apply the missing wedge Ewald's sphere correciton & convert to q-space
+    1. Use pygix to apply the missing wedge Ewald's sphere correction & convert to q-space
     2. Generate 2D plots of Qz vs Qxy corrected detector images
     3. Generate 2d plots of Q vs Chi images, with the option to apply the sin(chi) correction
     4. etc.
-    
 """
 
 import xarray as xr
@@ -14,6 +13,118 @@ import fabio # fabio package for .edf imports
 from PyHyperScattering.IntegrationUtils import DrawMask
 from tqdm.auto import tqdm 
 
+class Transform:
+    """
+    Class for transforming GIWAXS data into different formats.
+    
+    Attributes:
+        poniPath (str): Path to .poni file for converting to q-space & applying missing wedge correction
+        maskPath (str): Path to the mask file to use for the conversion
+        inplane_config (str): The configuration of the inplane. Default is 'q_xy'.
+    """
+
+    def __init__(self, poniPath, maskPath, inplane_config='q_xy'):
+        self.poniPath = poniPath
+        self.maskPath = maskPath
+        self.inplane_config = inplane_config
+
+    def load_mask(self, da):
+        """Load the mask file based on its file type."""
+        try:
+            if self.maskPath.suffix == '.json':
+                draw = DrawMask(da)  
+                draw.load(self.maskPath)
+                return draw.mask
+            elif self.maskPath.suffix == '.edf':
+                return fabio.open(self.maskPath).data
+            else:
+                raise ValueError(f"Unsupported file type: {self.maskPath.suffix}")
+        except Exception as e:
+            print(f"An error occurred while loading the mask file: {e}")
+
+    def add_time_coord(self, da):
+        """Add the 'time' coordinate if it exists in the original data array."""
+        if 'time' in da.coords:
+            da = da.assign_coords({'time': float(da.time)})
+            da = da.expand_dims(dim={'time': 1})
+        return da
+
+    def pg_convert(self, da):
+        """
+        Converts raw GIWAXS detector image to q-space data. Returns two DataArrays, Qz vs Qxy & Q vs Chi
+        
+        Inputs: Raw GIWAXS DataArray
+        Outputs: Cartesian & Polar DataArrays
+        """
+
+        # Initialize pygix transform object
+        pg = pygix.Transform()
+        pg.load(str(self.poniPath))
+        pg.sample_orientation = 3
+        pg.incident_angle = float(da.incident_angle[2:])
+
+        # load mask
+        mask = self.load_mask(da)
+
+        recip_data, qxy, qz = pg.transform_reciprocal(da.data,
+                                                      method='bbox',
+                                                      unit='A',
+                                                      mask=mask,
+                                                      correctSolidAngle=True)
+        
+        recip_da = xr.DataArray(data=recip_data,
+                                dims=['q_z', self.inplane_config],
+                                coords={
+                                    'q_z': ('q_z', qz, {'units': '1/Å'}),
+                                    self.inplane_config: (self.inplane_config, qxy, {'units': '1/Å'})
+                                },
+                                attrs=da.attrs)
+
+        caked_data, qr, chi = pg.transform_image(da.data, 
+                                                 process='polar',
+                                                 method = 'bbox',
+                                                 unit='q_A^-1',
+                                                 mask=mask,
+                                                 correctSolidAngle=True)
+
+        caked_da = xr.DataArray(data=caked_data,
+                            dims=['chi', 'qr'],
+                            coords={
+                                'chi': ('chi', chi, {'units': '°'}),
+                                'qr': ('qr', qr, {'units': '1/Å'})
+                            },
+                            attrs=da.attrs)
+        caked_da.attrs['inplane_config'] = self.inplane_config
+
+        recip_da = self.add_time_coord(recip_da)
+        caked_da = self.add_time_coord(caked_da)
+
+        return recip_da, caked_da
+
+    def pg_convert_series(self, da):
+        """
+        Converts raw GIWAXS DataArray to q-space and returns Cartesian & Polar DataArrays
+
+        Inputs: Raw GIWAXS DataArray with a time dimension
+        Outputs: 2 DataArrays in q-space with dimensions (q_z, inplane_config (default is q_xy), time) and (chi, qr, time)
+        """
+        recip_das = []
+        caked_das = []
+        for time in tqdm(da.time):
+            da_slice = da.sel(time=float(time))
+            recip_da_slice, caked_da_slice = self.pg_convert(da=da_slice)
+            recip_das.append(recip_da_slice)
+            caked_das.append(caked_da_slice)
+            
+        recip_da_series = xr.concat(recip_das, 'time')
+        caked_da_series = xr.concat(caked_das, 'time')
+        
+        return recip_da_series, caked_da_series
+
+
+
+# -- old Tranform class, refactored: (07/17/23)
+'''
 class Transform:
 
     def __init__(self, poniPath, maskPath, inplane_config='q_xy'):
@@ -48,11 +159,6 @@ class Transform:
             mask = fabio.open(self.maskPath).data # load mask file
         else:
             print(f"Unsupported file type: {self.maskPath.suffix}")
-
-        # # Load PyHyper-drawn mask
-        # draw = DrawMask(da)
-        # draw.load(self.maskPath)
-        # mask = draw.mask
 
         recip_data, qxy, qz = pg.transform_reciprocal(da.data,
                                                       method='bbox',
@@ -111,8 +217,10 @@ class Transform:
         caked_da_series = xr.concat(caked_das, 'time')
         
         return recip_da_series, caked_da_series
+'''
 
-
+# -- old pg_convert script (07/17/23)
+'''
 # def pg_convert(da, poniPath, maskPath, inplane_config='q_xy'):
 #     """
 #     Converts raw GIWAXS detector image to q-space data. Returns two DataArrays, Qz vs Qxy & Q vs Chi
@@ -193,3 +301,4 @@ class Transform:
 #     caked_da_series = xr.concat(caked_das, 'time')
     
 #     return recip_da_series, caked_da_series
+'''
