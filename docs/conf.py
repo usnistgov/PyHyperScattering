f8395f11edc4b36bd995970fa653e7cb0dba399b
Patel <bbp@NIST.GOV>
2023-09-11 13:59:29 -0400

    Attempt pydata theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,21 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
+
+sys.path.insert(0, os.path.abspath("../src/"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PyHyperScattering'
-copyright = ': Official Contribution of the US Government.  Not subject to copyright in the United States.'
-author = 'Peter Beaucage'
+project = "PyHyperScattering"
+copyright = (
+    ": Official Contribution of the US Government.  Not subject to copyright in the United States."
+)
+author = "Peter Beaucage"
 
 # The full version, including alpha/beta/rc tags
 from PyHyperScattering import __version__
+
 release = __version__
 
 
@@ -32,18 +36,18 @@ release = __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.coverage',
-'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,12 +55,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 import sys
-sys.path.append('../src/')
+
+sys.path.append("../src/")
