28c1a079eb7d92344e84e1b2fba0560d9de7fce4
Andrew Levin <andrew.levin@colorado.edu>
2024-04-23 14:33:15 -0600

    update if statement in ponifile loader

--- src/PyHyperScattering/PFGeneralIntegrator.py
+++ src/PyHyperScattering/PFGeneralIntegrator.py
@@ -517,7 +517,7 @@ class PFGeneralIntegrator():
         self.pixel1 = ponifile.detector.pixel1
         self.pixel2 = ponifile.detector.pixel2
         
-        if not self.mask and not raw_xr:  # if both are None
+        if self.mask is None and raw_xr is None: 
             self.mask = np.zeros((len(raw_xr.pix_y),len(raw_xr.pix_x)))
             warnings.warn(f'Since mask was none, creating an empty mask with shape {self.mask.shape}',stacklevel=2)
         self.recreateIntegrator()
