79c758be7949cbd5759c7bf4f504a9a479b5acf5
Andrew Levin <andrew.levin@colorado.edu>
2024-04-23 13:15:36 -0600

    remove legacy giwaxs import in util.py

--- src/PyHyperScattering/util.py
+++ src/PyHyperScattering/util.py
@@ -5,4 +5,3 @@ from PyHyperScattering import IntegrationUtils
 #from PyHyperScattering import Nexus empty module as of 0.0.6-dev69
 from PyHyperScattering import FileIO
 from PyHyperScattering import PlotTools
-from PyHyperScattering.GIWAXS import Transform
