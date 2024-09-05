0a2ad04fae66086ff0330fdb299fef64faac00eb
Andrew Levin <andrew.levin@colorado.edu>
2024-09-05 17:52:59 -0400

    plot_ISI pulls defaults from new plot_ROIs attribute, move save option inside function

--- src/PyHyperScattering/PlotTools.py
+++ src/PyHyperScattering/PlotTools.py
@@ -2,6 +2,7 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
+import pathlib
 
 @xr.register_dataset_accessor('pt')
 @xr.register_dataarray_accessor('pt')
@@ -23,21 +24,44 @@ class PlotTools():
         '''
         return self._obj.plot(xscale='log',yscale='log')
 
-    def plot_ISI(self, pol, chi_width, q_slice, e_slice, sample_name):
+    def plot_ISI(self, 
+                 pol=None, 
+                 chi_width=None, 
+                 q_slice=None, 
+                 e_slice=None, 
+                 sample_name=None,
+                 save=True):
         """
         Plot the integrated scattered intensity:
         
-        Inputs: 
+        Inputs: (default will be to pull from 'plot_ROIs' attribute)
             pol (int): X-ray polarization to determine para/perp chi regions
             chi_width (int): width of chi wedge for para/perp slices
             q_slice (slice): q range entered as slice object
             e_slice (slice): energy range entered as slice object 
             sample_name (str): sample name to be included in plot title  
+            save (bool, default True): save figure to new folder in notebook directory
 
         Returns:
             fig: matplotlib figure object of the ISI plot
             ax: matplotlib axes object of the ISI plot
         """
+
+        # Load default plot ROI values from 'plot_ROIs' attribute / dictionary
+        # Can be overwritten in the function call
+        if pol is None:
+            pol = int(self._obj.polarization)
+        if chi_width is None:
+            chi_width = self._obj.plot_ROIs['chi_width']
+        if q_slice is None:
+            q_tup = self._obj.plot_ROIs['q_range']
+            q_slice = slice(q_tup[0], q_tup[1])
+        if e_slice is None:
+            e_tup = self._obj.plot_ROIs['energy_range']
+            e_slice = slice(e_tup[0], e_tup[1])
+        if sample_name is None:
+            sample_name = str(self._obj.sample_name.values)
+
         # Slice parallel & perpendicular DataArrays (for polarization = 0)
         if pol == 0:
             para_DA = self._obj.rsoxs.slice_chi(180, chi_width=(chi_width/2))
@@ -62,6 +86,13 @@ class PlotTools():
         ax.legend(loc='upper right', fontsize=14)
         ax.grid(axis='x', which='both')
         plt.subplots_adjust(top=0.86, bottom=0.2, left=0.2)
+
+        # Save plot if true (saves to notebook working directory)
+        if save:
+            filename = f'{sample_name}_chi-{chi_width}_q-{q_slice.start}-{q_slice.stop}_energy-{e_slice.start}-{e_slice.stop}_.png'
+            savePath = pathlib.Path.cwd().joinpath('ISI_plots')
+            savePath.mkdir(exist_ok=True)
+            fig.savefig(savePath.joinpath(filename))
 
         return fig, ax
 
