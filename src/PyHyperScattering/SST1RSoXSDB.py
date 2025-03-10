489d41fc40ee84b64ff6fc418734e662be57119b
PriyankaKetkarBNL <155195376+PriyankaKetkarBNL@users.noreply.github.com>
2025-03-10 08:40:22 -0400

    Updated config identifying condition

--- src/PyHyperScattering/SST1RSoXSDB.py
+++ src/PyHyperScattering/SST1RSoXSDB.py
@@ -1000,7 +1000,7 @@ class SST1RSoXSDB:
                 md["beamcenter_y"] = run.start["RSoXS_SAXS_BCY"]
                 md["sdd"] = run.start["RSoXS_SAXS_SDD"]
 
-        elif start["RSoXS_Config"] == "WAXS":
+        elif "WAXS" in start["RSoXS_Config"]:
             md["rsoxs_config"] = "waxs"
             if (meas_time > datetime.datetime(2020, 11, 16)) and (
                 meas_time < datetime.datetime(2021, 1, 15)
@@ -1016,9 +1016,9 @@ class SST1RSoXSDB:
                 md["beamcenter_y"] = 549.76
                 md["sdd"] = 34.5  # GUESS; SOMEONE SHOULD CONFIRM WITH A BCP MAYBE??
             else:
-                md["beamcenter_x"] = 467.5#run.start["RSoXS_WAXS_BCX"]  # 399 #
-                md["beamcenter_y"] = 513.4 #run.start["RSoXS_WAXS_BCY"]  # 526
-                md["sdd"] = 39.19#run.start["RSoXS_WAXS_SDD"]
+                md["beamcenter_x"] = run.start["RSoXS_WAXS_BCX"]  # 399 #
+                md["beamcenter_y"] = run.start["RSoXS_WAXS_BCY"]  # 526
+                md["sdd"] = run.start["RSoXS_WAXS_SDD"]
 
         else:
             md["rsoxs_config"] = "unknown"
